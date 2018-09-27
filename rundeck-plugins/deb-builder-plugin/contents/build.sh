ba4372584c85055a0cd727e358f8be4371a6a2c6
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2018-09-26 17:09:10 -0700

    Add log highlighting

--- rundeck-plugins/deb-builder-plugin/contents/build.sh
+++ rundeck-plugins/deb-builder-plugin/contents/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 
 S3_BASE=rundeck-playground/consul/
 
@@ -13,17 +13,26 @@ URL="https://releases.hashicorp.com/consul/${VERSION}/consul_${VERSION}_linux_am
 
 mkdir -p "$_pkgdir"
 
+echo "INFO - Downloading $URL"
 curl -sOL "$URL"
+
+echo "INFO - Packaging $NAME version $VERSION"
 unzip $(basename "$URL")
+touch $BIN_NAME
 mkdir -p "${_pkgdir}/usr/bin"
 install "$BIN_NAME" "${_pkgdir}/usr/bin"
 cd ${_pkgdir}
 DEB_FILE=$(fpm -s dir -t deb --name "$NAME" --version "$VERSION" . | ruby -e 'puts eval(STDIN.read)[:path]')
-S3_URL="s3://${S3_BASE%%/*}/${DEB_FILE}"
 
+echo "INFO - Uploading $DEB_FILE to $S3_URL"
+S3_URL="s3://${S3_BASE%%/*}/${DEB_FILE}"
 export AWS_ACCESS_KEY_ID=$1
+set +x
 export AWS_SECRET_ACCESS_KEY=$2
+set -x
 aws s3 cp --quiet "$DEB_FILE" "$S3_URL"
+
+echo "INFO - Generating presigned url for $S3_URL"
 echo -n "RUNDECK:DATA:DOWNLOAD_URL = "
 aws s3 presign "$S3_URL"
 echo "RUNDECK:DATA:DEB_FILE = $DEB_FILE"
